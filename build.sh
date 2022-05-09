3746ec2bf735560d561b57c4a2b3c76d8c182a2b
Morten Daniel Fornes <morten@fusetools.com>
2022-05-09 14:05:44 +0700

    create xcframework

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ set -e
 
 INA="device/build/Debug-iphoneos/UnityFramework.framework"
 INB="simulator/build/Debug-iphonesimulator/UnityFramework.framework"
-OUT="build/UnityFramework.framework"
+OUT="ios/UnityFramework.xcframework"
+
+rm -rf "$OUT" ios.tgz
+mkdir -p "$OUT"
 
 xcodebuild -project "simulator/Unity-iPhone.xcodeproj" -target "UnityFramework" -configuration "Debug" -sdk "iphonesimulator"
 xcodebuild -project "device/Unity-iPhone.xcodeproj" -target "UnityFramework" -configuration "Debug" -sdk "iphoneos"
+xcodebuild -create-xcframework -framework "$INA" -framework "$INB" -output "$OUT"
 
-rm -rf "$OUT"
-mkdir -p "$OUT"
-cp -R "$INB"/* "$OUT"
-lipo -create "$INA/UnityFramework" "$INB/UnityFramework" -output "$OUT/UnityFramework"
+tar -czvf ios.tgz ios
+du -h ios.tgz
